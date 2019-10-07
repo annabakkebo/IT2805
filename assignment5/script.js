ca7de4b4f2730ea98af811d5b680cfb75b513040
annabakkebo <annabakk@stud.ntnu.no>
2019-10-07 16:13:47 +0200

    part5 and 6

--- assignment5/script.js
+++ assignment5/script.js
@@ -8,20 +8,39 @@ for (i = 1; i<21; i++){
 console.log('PART 3')
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+for(i of numbers){
+    if (i % 3==0){
+        if (i%5==0){
+            console.log("eplekake")
+        }
+        else{
+            console.log("eple")
+        }
+    }
+    else if (i % 5 ==0){
+        console.log("kake")
+    }
+    else {
+        console.log(i)
+    }
+}
 
 /* Part 4 */
+document.getElementById('title').innerText = 'Hello, JavaScript';
 
 /* Part 5 */
 function changeDisplay () {
-
+    document.getElementById('magic').style.display ="none";
 }
 
 function changeVisibility () {
-
+    document.getElementById('magic').style.visibility="hidden";
+    document.getElementById('magic').style.display="block";
 }
 
 function reset () {
-
+    document.getElementById('magic').style.visibility="visible";
+    document.getElementById('magic').style.display="block";
 }
 
 /* Part 6 */
@@ -38,3 +57,11 @@ const technologies = [
     'Bootstrap',
     'Node.js'
 ];
+for(i of technologies){
+    var node = document.createElement("LI");
+    /*var textnode = document.createTextNode(i);
+    * node.appendChild(textnode);
+    * This can also be used, but I found it easier with the one stated below.*/
+    node.innerHTML = i;
+    document.getElementById('tech').appendChild(node);
+}
